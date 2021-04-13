5cfdbc326e7ca0f79e46f38d971f7689dcabffa9
Se7enSe7enSe7en <50391562+Se7enSe7enSe7en@users.noreply.github.com>
2021-04-13 17:51:47 +0800

    13-04-2021-5:51pm

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">click me</button>
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" :weird_array="weird_array" theme="sale" @close="toggleModal" />
+  </div>
+  <button @click="toggleModal">open modal</button>
+  
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import Modal from "./components/Modal.vue"
 
 @Options({
-  components: {
-    HelloWorld,
+  components: { 
+    Modal, 
+  },
+  data() {
+    return{
+      title: "My First Vue App",
+      header: "Sign up for the Giveaway!",
+      text: "Grab your ninja swag for half price!",
+      weird_array: [2, 7, 4, 3],
+      showModal: false
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active") // [!] [class: active] is a class that adds a CSS effect when activated
+      this.$refs.name.focus() // [!] [.focus()]when button is clicked it will focus the input box
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
   },
 })
 export default class App extends Vue {}
@@ -23,5 +49,10 @@ export default class App extends Vue {}
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
